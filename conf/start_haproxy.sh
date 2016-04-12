87cec9ae0aa447cd5d6ba3f40d0d913dabd2fcda
100026625 <Benoit.Laurent@ge.com>
2016-04-12 16:38:32 +0200

    delete spec

--- conf/start_haproxy.sh
+++ conf/start_haproxy.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 echo "About to configure haproxy"
 sed -i "s/\$PORT /${PORT}/g" haproxy.cfg
 echo "haproxy configure:"
 cat haproxy.cfg
 echo "About to start haproxy"
-./haproxy -f haproxy.cfg -db
+./haproxy -f haproxy.cfg -d
