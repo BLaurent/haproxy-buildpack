7c83304f2d551bbfed86949b6c5bd9c0de5967d8
100026625 <Benoit.Laurent@ge.com>
2016-04-12 16:01:16 +0200

    debug boot script

---
+++ conf/start_haproxy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "About to configure haproxy"
+sed -i "s/\$PORT /${PORT}/g" haproxy.cfg
+echo "haproxy configure:"
+cat haproxy.cfg
+echo "About to start haproxy"
+./haproxy -f haproxy.cfg -db
