6e3cca1dc297ba02713042fd7a8a3dcd73bf9ab9
100026625 <Benoit.Laurent@ge.com>
2016-04-14 10:39:07 +0200

    fix sed expression

--- conf/start_haproxy.sh
+++ conf/start_haproxy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail   # don't ignore exit codes when piping output
+set -o nounset    # fail on unset variables
+
 echo "About to configure haproxy"
 sed -i 's/$PORT/'${VCAP_APP_PORT}'/g' haproxy.cfg
 echo "haproxy configure:"
