b432f1ea707e4d5ce360c0aad526bbbd7e1241d7
100026625 <Benoit.Laurent@ge.com>
2016-04-14 11:00:59 +0200

    debug

--- conf/start_haproxy.sh
+++ conf/start_haproxy.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
-
-set -e
-set -o pipefail   # don't ignore exit codes when piping output
-set -o nounset    # fail on unset variables
-
-env
-
 echo "About to configure haproxy"
 sed -i 's/$PORT/'${VCAP_APP_PORT}'/g' haproxy.cfg
 echo "haproxy configure:"
